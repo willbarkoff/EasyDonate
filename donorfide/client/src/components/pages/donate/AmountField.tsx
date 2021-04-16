43bad1872e196b52bf220f77e3af79f31a793e75
William Barkoff <william@barkoffusa.com>
2021-04-16 00:19:08 -0400

    fix floating point issues

--- donorfide/client/src/components/pages/donate/AmountField.tsx
+++ donorfide/client/src/components/pages/donate/AmountField.tsx
@@ -11,7 +11,7 @@ interface amountFieldProps {
 export default function AmountField(props: amountFieldProps): JSX.Element {
 	return <div className="amount-field-wrapper">
 		<div className="control has-icons-left has-icons-right amount-field">
-			<input className="input is-large" type="number" placeholder="Amount"
+			<input className="input is-large" type="number" placeholder="Amount" min={0.50} max={999999.99} step={0.01}
 				onChange={(event) => props.onChange(event.target.valueAsNumber * 100)} value={props.value / 100} />
 			<span className="icon is-medium is-left">
 				<FontAwesomeIcon icon={faDollarSign} />
--- donorfide/client/src/components/pages/donate/DonatePage.tsx
+++ donorfide/client/src/components/pages/donate/DonatePage.tsx
@@ -3,21 +3,42 @@ import Page from "../Page";
 import Hero from "../../ui/Hero";
 import { OrgContext } from "../../App";
 import AmountField from "./AmountField";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 export default function DonatePage(): JSX.Element {
 	const [amount, setAmount] = React.useState(500);
 
+	const [redirect, setRedirect] = React.useState(false);
+
+	function onSubmit(e: React.FormEvent) {
+		e.preventDefault();
+
+		setRedirect(true);
+	}
+
+	function updateAmount(newAmount: number) {
+		if (isNaN(newAmount)) {
+			setAmount(0);
+			return;
+		}
+
+		setAmount(Math.round(newAmount));
+	}
+
 	return <OrgContext.Consumer>
 		{org => <Page>
+			{redirect && <Redirect to={`/donate/${amount}`} />}
 			<Hero style="primary" title={org.donate_page_title || "Donate"}
 				subtitle={org.donate_page_subtitle || `Contribute to ${org.name}`} center />
 			<section className="section">
-				{org.home_page_text && <p className="block has-text-centered">{org.home_page_text}</p>}
-				<div className="container has-text-centered">
-					<AmountField onChange={setAmount} value={amount} />
-					<Link to={`/donate/${amount}`} className="button is-primary m-5">Continue</Link>
-				</div>
+				<form onSubmit={onSubmit}>
+					{org.home_page_text && <p className="block has-text-centered">{org.home_page_text}</p>}
+					<div className="container has-text-centered">
+						<AmountField onChange={updateAmount} value={amount} />
+						{/* Use an input so that the browser can handle validation. */}
+						<input type="submit" className="button is-primary m-5" value="Continue" />
+					</div>
+				</form>
 			</section>
 		</Page>}
 	</OrgContext.Consumer>;
