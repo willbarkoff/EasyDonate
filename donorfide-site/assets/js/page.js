7ebd1d2fe4f98e1027b2744eda2963af873b20fd
William Barkoff <william@barkoffusa.com>
2021-07-12 13:31:28 -0400

    docs: mobile improvements

---
+++ donorfide-site/assets/js/page.js
@@ -0,0 +1,7 @@
+var burger = document.querySelector('.navbar-burger');
+var menu = document.querySelector('.navbar-menu');
+
+burger.addEventListener('click', function () {
+	burger.classList.toggle('is-active');
+	menu.classList.toggle('is-active');
+});
